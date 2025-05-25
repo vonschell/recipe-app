9591ab0585ed674894fb26a374e3bdb9da54c827
vonschell <vonschell02@gmail.com>
2025-05-25 16:20:10 +0200

    Add recipe model with fields for title, ingredients, instructions, and more

--- api/api.py
+++ api/api.py
@@ -7,5 +7,19 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
 db = SQLAlchemy(app)
 
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(500), nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=True,
+                            default='Delicious. You need to try it!')
+    image_url = db.Column(db.String(500), nullable=True,
+                          default="https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+    servings = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', servings={self.servings})"
+
 if __name__ == '__main__':
     app.run(debug=True)
