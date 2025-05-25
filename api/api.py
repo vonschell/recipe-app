87031776ed5154fe1f7a3902adad2accc052a92d
vonschell <vonschell02@gmail.com>
2025-05-25 16:13:34 +0200

    Set up SQLAlchemy and SQLite database connection in api.py

--- api/api.py
+++ api/api.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
