dbbff247f96a59eb3715b7a68d24345359e279c4
vonschell <vonschell02@gmail.com>
2025-05-26 15:31:12 +0200

    Forgot to save the creation of RecipeExcert first before commiting

---
+++ src/components/RecipeExcerpt.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RecipeExcerpt = ({ recipe }) => {
+    return (
+        <article className="recipe-card">
+            <figure>
+                <h2>
+                    <p className='flex-spacing'>
+                        <button></button>
+                    </p>
+                </h2>
+            </figure>
+        </article>
+    );
+};
