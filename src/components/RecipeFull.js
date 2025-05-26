1858b717334f4ca651071f4edbe45cc297ab8423
vonschell <vonschell02@gmail.com>
2025-05-26 18:07:41 +0200

    Complete RecipeFull component with full recipe details, ingredients list, and Close button functionality

--- src/components/RecipeFull.js
+++ src/components/RecipeFull.js
@@ -1,36 +1,41 @@
 import React from "react";
+import { X } from "react-feather";
 
-const RecipeFull = () => {
+const RecipeFull = ({ selectedRecipe, handleUnselectRecipe }) => {
     return (
         <div className='recipe-details'>
     <article>
       <header>
         <figure>
-          <img />
+          <img alt={selectedRecipe.title} src={selectedRecipe.image_url}/>
         </figure>
-        <h2>{}</h2>
+        <h2>{selectedRecipe.title}</h2>
         <div className='button-container'>
           <button className='edit-button'>Edit</button>
-          <button className='cancel-button'>
-            Close
+          <button className='cancel-button' onClick={handleUnselectRecipe}>
+            <X /> Close
           </button>
           <button className='delete-button'>Delete</button>
         </div>
       </header>
  
       <h3>Description:</h3>
-      <p>{}</p>
+      <p>{selectedRecipe.description}</p>
  
       <h3>Ingredients:</h3>
  
       <ul className='ingredient-list'>
- 
+        {selectedRecipe.ingredients.split(",").map((ingredient, index) => (
+            <li className='ingredient' key={index}>
+                {ingredient}
+                </li>
+        ))} 
       </ul>
       <h3>Instructions:</h3>
  
-      <pre className='formatted-text'>{}</pre>
+      <pre className='formatted-text'>{selectedRecipe.instructions}</pre>
  
-      <h3>Servings: {}</h3>
+      <h3>Servings: {selectedRecipe.servings}</h3>
     </article>
 </div>
     )
