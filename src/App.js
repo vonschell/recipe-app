094c09c308bf06ddbabd2da69e2906a3d01ef592
vonschell <vonschell02@gmail.com>
2025-05-26 15:13:43 +0200

    Fetch recipes from /api/recipes and update state using useEffect

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
-  
+
+  useEffect(() => {
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes");
+        if (response.ok) {
+          const data = await response.json();
+          setRecipes(data);
+        } else {
+          console.log("Failed to fetch recipes");
+        }
+      } catch (e) {
+        console.error("Error fetching recipes:", e);
+        console.log("An error occurred while fetching recipes. Please try again later.");
+      }
+    };
+    fetchAllRecipes();
+  }, []);
+
   return (
     <div className='recipe-app'>
       <Header />
       <p>Your recipes here! </p>
+      {JSON.stringify(recipes)}
     </div>
   );
 }
