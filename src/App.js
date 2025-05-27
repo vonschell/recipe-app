5d19fe3852c5ef6cffbb03e935f225b6e0e446b9
vonschell <vonschell02@gmail.com>
2025-05-27 17:03:19 +0200

    Pass showRecipeForm function as a prop to header component

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
 
   return (
     <div className="recipe-app">
-      <Header showNewRecipeForm={showNewRecipeForm}/>
+      <Header showRecipeForm={showRecipeForm}/>
       
       {showNewRecipeForm && (
         <NewRecipeForm
