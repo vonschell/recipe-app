8063451136c57738465eeaf3d1a778b1ef3b8747
vonschell <vonschell02@gmail.com>
2025-05-28 18:06:38 +0200

    Destructure selectedRecipe and handleCancel in EditRecipeForm and bind cancel button and populate form fields with selectedRecipe data

--- src/components/EditRecipeForm.js
+++ src/components/EditRecipeForm.js
@@ -1,27 +1,64 @@
 import React from "react";
 
-const EditRecipeForm = () => {
-  return (
-    <div className='recipe-form'>
-      <h2>Edit "{}"</h2>
-      <button className='cancel-button'>Cancel</button>
-      <form>
-        <label>Title</label>
-        <input type='text' name='title' value='' onChange='' required />
-        <label>Ingredients</label>
-        <textarea name='ingredients' value='' onChange='' required />
-        <label>Instructions</label>
-        <textarea name='instructions' value='' onChange='' required />
-        <label>Description</label>
-        <textarea name='description' value='' onChange='' required />
-        <label>Image</label>
-        <input type='text' name='image_url' value='' onChange='' required />
-        <label>Servings</label>
-        <input type='number' name='servings' value='' onChange='' required />
-        <button type='submit'>Update Recipe</button>
-      </form>
-    </div>
-  );
+const EditRecipeForm = ({ selectedRecipe, handleCancel }) => {
+    return (
+        <div className="recipe-form">
+            <h2>Edit "{selectedRecipe.title}"</h2>
+            <button className="cancel-button" onClick={handleCancel}>
+                Cancel
+            </button>
+
+            <form onSubmit={(e) => handleUpdateRecipe(e, selectedRecipe)}>
+                <label>Title</label>
+                <input
+                    type="text"
+                    name="title"
+                    value={selectedRecipe.title}
+                    onChange={(e) => onUpdateForm(e, "update")}
+                    required
+                />
+                <label>Ingredients</label>
+                <textarea
+                    name="ingredients"
+                    value={selectedRecipe.ingredients}
+                    placeholder="Add ingredients separated by commas - i.e. Flour, sugar, almonds"
+                    onChange={(e) => onUpdateForm(e, "update")}
+                    required
+                />
+                <label>Instructions</label>
+                <textarea
+                    name="instructions"
+                    value={selectedRecipe.instructions}
+                    onChange={(e) => onUpdateForm(e, "update")}
+                    required
+                />
+                <label>Description</label>
+                <textarea
+                    name="description"
+                    value={selectedRecipe.description}
+                    onChange={(e) => onUpdateForm(e, "update")}
+                    required
+                />
+                <label>Image</label>
+                <input
+                    type="text"
+                    name="image_url"
+                    value={selectedRecipe.image_url}
+                    onChange={(e) => onUpdateForm(e, "update")}
+                    required
+                />
+                <label>Servings</label>
+                <input
+                    type="number"
+                    name="servings"
+                    value={selectedRecipe.servings}
+                    onChange={(e) => onUpdateForm(e, "update")}
+                    required
+                />
+                <button type="submit">Update Recipe</button>
+            </form>
+        </div>
+    );
 };
 
 export default EditRecipeForm;
