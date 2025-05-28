deded25cd7f4653aa5d3939115ba825be0f22d4f
vonschell <vonschell02@gmail.com>
2025-05-28 15:29:43 +0200

    Add response handling and reset form after recipe submit

--- src/App.js
+++ src/App.js
@@ -67,7 +67,23 @@ function App() {
           "content-type": "application/json"
         },
         body: JSON.stringify(newRecipe)
-      });
+      }); 
+
+      if (response.ok) {
+        const date = await response.json();
+        setRecipes([...recipes, date.recipe]);
+
+        console.log("recipe added successfully");
+
+        setShowNewRecipeForm(false);
+        setNewRecipe({
+          title: "",
+          ingredients: "",
+          instructions: "",
+          servings: 1,
+          description: "",
+          image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        });
 
   return (
     <div className="recipe-app">
