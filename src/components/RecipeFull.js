cda3ea1eff8f9f7d80c130aa0651728b27b33fca
vonschell <vonschell02@gmail.com>
2025-05-28 18:12:30 +0200

    Add handleUpdateRecipe prop to EditRecipeForm in RecipeFull.js to enable recipe updates

--- src/components/RecipeFull.js
+++ src/components/RecipeFull.js
@@ -1,59 +1,64 @@
 import React from "react";
 import { X } from "react-feather";
-import EditRecipeForm from "./EditRecipeForm"; 
+import EditRecipeForm from "./EditRecipeForm";
 
 const RecipeFull = ({ selectedRecipe, handleUnselectRecipe }) => {
-  
   const [editing, setEditing] = React.useState(false);
 
   const handleCancel = () => {
     setEditing(false);
-  }
-  
+  };
+
   return (
-        <div className='recipe-details'>
-          {editing ? (
-            <EditRecipeForm 
-            selectedRecipe={selectedRecipe}
-            onUpdateForm={onUpdateForm}
-            handleCancel={handleCancel}
-            />
-          ) : (
-    <article>
-      <header>
-        <figure>
-          <img alt={selectedRecipe.title} src={selectedRecipe.image_url}/>
-        </figure>
-        <h2>{selectedRecipe.title}</h2>
-        <div className='button-container'>
-          <button className='edit-button' onClick={() => setEditing(true)}> Edit</button>
-          <button className='cancel-button' onClick={() => handleUnselectRecipe(selectedRecipe)}>
-            <X /> Close
-          </button>
-          <button className='delete-button'>Delete</button>
-        </div>
-      </header>
- 
-      <h3>Description:</h3>
-      <p>{selectedRecipe.description}</p>
- 
-      <h3>Ingredients:</h3>
- 
-      <ul className='ingredient-list'>
-        {selectedRecipe.ingredients.split(",").map((ingredient, index) => (
-            <li className='ingredient' key={index}>
+    <div className="recipe-details">
+      {editing ? (
+        <EditRecipeForm
+          selectedRecipe={selectedRecipe}
+          onUpdateForm={onUpdateForm}
+          handleCancel={handleCancel}
+          handleUpdateRecipe={handleUpdateRecipe}
+        />
+      ) : (
+        <article>
+          <header>
+            <figure>
+              <img alt={selectedRecipe.title} src={selectedRecipe.image_url} />
+            </figure>
+            <h2>{selectedRecipe.title}</h2>
+            <div className="button-container">
+              <button className="edit-button" onClick={() => setEditing(true)}>
+                Edit
+              </button>
+              <button
+                className="cancel-button"
+                onClick={() => handleUnselectRecipe(selectedRecipe)}
+              >
+                <X /> Close
+              </button>
+              <button className="delete-button">Delete</button>
+            </div>
+          </header>
+
+          <h3>Description:</h3>
+          <p>{selectedRecipe.description}</p>
+
+          <h3>Ingredients:</h3>
+          <ul className="ingredient-list">
+            {selectedRecipe.ingredients.split(",").map((ingredient, index) => (
+              <li className="ingredient" key={index}>
                 {ingredient}
-                </li>
-        ))} 
-      </ul>
-      <h3>Instructions:</h3>
- 
-      <pre className='formatted-text'>{selectedRecipe.instructions}</pre>
- 
-      <h3>Servings: {selectedRecipe.servings}</h3>
-    </article>
-</div>
-    )
-}
+              </li>
+            ))}
+          </ul>
+
+          <h3>Instructions:</h3>
+          <pre className="formatted-text">{selectedRecipe.instructions}</pre>
+
+          <h3>Servings: {selectedRecipe.servings}</h3>
+        </article>
+      )}
+    </div>
+  );
+};
 
 export default RecipeFull;
