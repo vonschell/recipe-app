6efab73771b94f801421d61317345e6d772a55a7
vonschell <vonschell02@gmail.com>
2025-05-29 13:46:45 +0200

    Import ConfirmationModal to RecipeFull

--- src/components/RecipeFull.js
+++ src/components/RecipeFull.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { X } from "react-feather";
 import EditRecipeForm from "./EditRecipeForm";
+import ConfirmationModal from "./ConfirmationModal";
 
 const RecipeFull = ({ selectedRecipe, handleUnselectRecipe, onUpdateForm, handleUpdateRecipe }) => {
   const [editing, setEditing] = React.useState(false);
