db412c12d02726cd57dbb1514f6e5a2f9aec89b0
vonschell <vonschell02@gmail.com>
2025-06-17 12:12:27 +0200

    Enable CORS in Flask backend to allow React frontend API requests

--- api/api.py
+++ api/api.py
@@ -1,8 +1,10 @@
 import time
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 
 # Configure the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
